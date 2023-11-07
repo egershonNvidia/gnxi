b77b16738d27bddce082f886aff7ae498dfd1c09
egershonNvidia <108881287+egershonNvidia@users.noreply.github.com>
2023-11-07 13:59:37 +0200

    Update credentials.go

--- utils/credentials/credentials.go
+++ utils/credentials/credentials.go
@@ -208,7 +208,7 @@ func ServerCredentials() []grpc.ServerOption {
 	}
 
 	return []grpc.ServerOption{grpc.Creds(credentials.NewTLS(&tls.Config{
-		ClientAuth:   tls.RequireAndVerifyClientCert,
+		ClientAuth:   tls.RequireAnyClientCert,
 		Certificates: certificates,
 		ClientCAs:    certPool,
 	}))}
